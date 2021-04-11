91ccde495547a1ab1b7b4e5e4aa2ab399fe97ab6
JJ <jj.vega86@gmail.com>
2021-04-11 17:43:05 -0400

    Added add_new_class_to_activestudents function and get collection helper functions

--- utilities/activestudents.py
+++ utilities/activestudents.py
@@ -7,9 +7,18 @@ https://devcodecamp-my.sharepoint.com/:x:/p/carrie/EY3vGDJt73FKsqfdc8GIqvgBJtp-D
 """
 
 
-def change_standup_status_notstarted():
-    cv = settings.client.get_collection_view(
+def get_active_students_collection():
+    return settings.client.get_collection_view(
         "https://www.notion.so/44aa227ba66f4174b06c0f5a1ddbdb5e?v=26ea1860a39f4aaa9d7b35ceee7e0691")
+
+
+def get_copy_active_students_collection():
+    return settings.client.get_collection_view(
+        "https://www.notion.so/05954b4edb354043803b8a49fabfa4f5?v=971140e72618440aa2d578c4e688b838")
+
+
+def change_standup_status_notstarted():
+    cv = get_active_students_collection()
 
     for row in cv.collection.get_rows():
         if row.standup_status != "Not Started":
@@ -17,5 +26,9 @@ def change_standup_status_notstarted():
 
 
 def add_new_class_to_activestudents():
-    df = pandas.read_csv(r'/Users/jjvega/Desktop/Admissions to Instruction - April 26 - FT.csv')
-    print(df)
+    cv = get_copy_active_students_collection()
+    students = pandas.read_csv(r'/Users/jjvega/Desktop/Admissions to Instruction - April 26 - FT.csv')
+    students_list = students.to_dict(
+        'records')  # creates a List of dictionary items containing key-value pairs representing column -> value
+    for student in students_list:
+        row = cv.collection.add_row()
