d76a10ea8c210bf979fd5797c5e7d8c7ba7e6db9
JJ <jj.vega86@gmail.com>
2021-04-11 15:56:29 -0400

    Modified store.py and client.py in Notion library to resolve bug that limits GET requests for large collections

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 # Main entry point of application
 from notion.client import NotionClient
-from notion.block import TodoBlock
 
 client = NotionClient(token_v2="281480126b050e8dbd5ba5e81bae40e6179e43e7cac0d2c40f65c18fe0c38580f44eaa7333ed9dcdfb7cbe0896b8943bcfe9ca6fe771276591162790eaf5cbb3662cde822d94c2982eaf9c57ec98")
 
 page = client.get_block("https://www.notion.so/Scratchpad-ed0a396de9ec40d38635f782458736db")
+cv = client.get_collection_view("https://www.notion.so/05954b4edb354043803b8a49fabfa4f5?v=971140e72618440aa2d578c4e688b838")
 
-for child in page.children:
-    print(child.title)
-
-print("Parent of {} is {}".format(page.id, page.parent.id))
-
-new_child = page.children.add_new(TodoBlock, title="Something to get done")
-print(new_child)
-new_child.checked = True
+for row in cv.collection.get_rows():
+    print(row.name)
 
