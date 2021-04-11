5ba720a1dc14dd1fac48d268a7b2ae7286d9efc5
JJ <jj.vega86@gmail.com>
2021-04-11 16:25:12 -0400

    Added config and utilities modules to project; refactored initial client setup to config/settings.py module

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 # Main entry point of application
-from notion.client import NotionClient
+from config import settings
 
-client = NotionClient(token_v2="281480126b050e8dbd5ba5e81bae40e6179e43e7cac0d2c40f65c18fe0c38580f44eaa7333ed9dcdfb7cbe0896b8943bcfe9ca6fe771276591162790eaf5cbb3662cde822d94c2982eaf9c57ec98")
+client = settings.client
 
 page = client.get_block("https://www.notion.so/Scratchpad-ed0a396de9ec40d38635f782458736db")
-cv = client.get_collection_view("https://www.notion.so/05954b4edb354043803b8a49fabfa4f5?v=971140e72618440aa2d578c4e688b838")
+lutetium = client.get_block(
+    "https://www.notion.so/Lutetium-1d399afb9aa1406ca97e2e81a822be97")  # By getting an instance of the Lutetium block, I can set the value of a relation property
+# to this object
+cv = client.get_collection_view(
+    "https://www.notion.so/44aa227ba66f4174b06c0f5a1ddbdb5e?v=26ea1860a39f4aaa9d7b35ceee7e0691")
 
 for row in cv.collection.get_rows():
-    print(row.name)
-
+    if row.standup_status != "Not Started":
+        row.standup_status = "Not Started"
